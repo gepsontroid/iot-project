2f3101fd5f79f21b448c004347e5157d270e19d1
gepsontroid <rishabhsoni15397@gmail.com>
2019-04-01 18:06:48 +0530

    Add files via upload

---
+++ LED_fading_alternate.ino
@@ -0,0 +1,32 @@
+int led1=9;
+int bri=0;
+int fade=5;
+int led2=10;
+int brd=225;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(led1,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+analogWrite(led1,bri);
+bri=bri+fade;
+if(bri<=0||bri>=255)
+{
+  fade=-fade;
+  
+}
+delay(100);
+
+
+  analogWrite(led2,brd);
+  brd=brd-fade;
+  if(brd<=0||brd>=255)
+  {
+    fade=-fade;
+  }
+  delay(100);
+  
+}
+
